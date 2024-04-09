b4c3db8ed7243f390c18f4f40cee753104f0ea21
DanielMeza8 <mezaromeroplantel14@gmail.com>
2024-04-09 08:57:54 -0600

    alfa

---
+++ contenido.php
@@ -0,0 +1,122 @@
+<?php
+
+    function contenido(){
+        $datos = array();
+        $datos[0] = "./img/cupraleon.jpg"."||"."Cupra Leon <br>$528.900"."||"."<ul>
+        <li>Motor: 4 cilindros turbo de 2.0 litros<br></li>
+        <li>Potencia máxima: 290 hp @ 5,900 rpm<br></li>
+        <li>Par máximo: 258 lb-pie @ 1,700 rpm<br></li>
+        <li>Transmisión: Automática de doble embrague de 6 vels.<br></li>
+        <li>Tracción: Delantera<br></li>
+        <li>Frenos: Disco / Disco<br></li>
+        <li>Peso: 1,421 kg<br></li>
+        <li>Velocidad máxima': 250 km/h<br></li>
+        <li>Aceleración de 0 a 100 km/h': 5.6 s<br></li>
+        <li>Suspensión delantera: Independiente tipo McPherson<br></li>
+        <li>Suspensión trasera: Independiente multibrazo<br></li>
+        <li>Longitud: 4,281 mm<br></li>
+        <li>Cajuela: 380 litros<br></li>
+        <li>Consumo en ciudad: 11.1 km/l<br></li>
+        <li>Emisiones de CO2': 151 g/km<br></li>
+        <li>Capacidad del tanque: 50 litros</li></ul>";
+        $datos[1] = "./img/jimmy.jpeg"."||"."Susuki Jimmy <br>$439.990"."||"."<ul>
+        <li>Motor: 4 cilindros de 1.5 litros<br></li>
+        <li>Potencia: 102 hp @ 6,000 rpm<br></li>
+        <li>Par: 96 lb-pie @ 4,000 rpm<br></li>
+        <li>Transmisión: Manual de 5 vels. + caja reductora<br></li>
+        <li>Tracción: Integral conectable<br></li>
+        <li>Frenos: Disco / tambor<br></li>
+        <li>Peso: 1,165 kg<br></li>
+        <li>Velocidad máxima: 145 km/h<br></li>
+        <li>Aceleración de 0 a 100 km/h: 14.3 s<br></li>
+        <li>Suspensión delantera: Eje rígido<br></li>
+        <li>Suspensión trasera: Eje rígido<br></li>
+        <li>Longitud: 3,480 mm<br></li>
+        <li>Cajuela: 85 litros<br></li>
+        <li>Consumo en ciudad: 12.4 km/l<br></li>
+        <li>Capacidad del tanque: 40 litros</li></ul>";
+        $datos[2] = "./img/mgGT.jpg"."||"."MG GT Alpha <br>$464.900"."||"."<ul>
+        <li>Motor: 1.5 litros turbo (4 cil.)<br></li>
+        <li>Potencia: 160 hp @ 5,600 RPM<br></li>
+        <li>Par: 185 lb-pie @ 1,750 - 4,000 RPM<br></li>
+        <li>Transmisión: Automática doble embrague de 7 cambios<br></li>
+        <li>Tracción: Delantera<br></li>
+        <li>Frenos: Disco / disco<br></li>
+        <li>Suspensión delantera: Independiente<br></li>
+        <li>Suspensión trasera: Barra de torsión<br></li>
+        <li>Longitud: 4,675 mm<br></li>
+        <li>Cajuela: 470 litros<br></li>
+        <li>Consumo en ciudad: 15.6 km/l</li></ul>";
+        $datos[3] = "./img/pegaut208.webp"."||"."Pegaut 208 GT <br>$484.000"."||"."<ul>
+        <li>Potencia: 100 hp @5,500 rpm<br></li>
+        <li>Torque: 205 NM @1,750 rpm<br></li>
+        <li>Cilindraje (cc): 1,199 (3 cilindros)<br></li>
+        <li>Velocidad Máxima: 187 km/h<br></li>
+        <li>Aceleración 0 a 100km: 11.2s<br></li>
+        <li>Tipo de combustible: Gasolina<br></li>
+        <li>Tracción: Delantera<br></li>
+        <li>Dirección: Asistencia eléctrica</li>
+        <li>Suspensión Del / Trasera: Pseudo MacPherson / Barra de torsión<br></li>
+        <li>Transmisión: Manual de 6 velocidades</li>
+        </ul>";
+        $datos[4] = "./img/polo.jpg"."||"."Wolksvagen Polo <br>$368.986"."||"."<ul>
+        <li>Motor 1.6 L 105 Hp / 153 Nm<br></li>
+        <li>Techo y carcasas de espejos exteriores pintados en color negro<br></li>
+        <li>Rines de aluminio de 15” en color negro<br></li>
+        <li>Sensor de lluvia con regulación automática de los limpiadores<br></li>
+        <li>Sensores de estacionamiento traseros<br></li>
+        <li>Espejo interior retrovisor antideslumbrante automático<br></li>
+        <li>Renovado en todos sus aspectos ideal para impactar</li></ul>";
+        $datos[5] = "./img/rangeRoverSport.jpg"."||"."Range Rover Sport <br>$2.203.987"."||"."<ul>
+        <li>Longitud: 4,94 metros<br></li>
+        <li> Velocidad máxima': 209 km/h<br></li>
+        <li> Anchura: 2,20 metros<br></li>
+        <li> Altura: 1,82 metros<br></li>
+        <li> Distancia entre ejes: 2,99 metros<br></li>
+        <li> Peso: 2.390 - 2.810 kilos<br></li>
+        <li> Maletero: 657 / 1.991 litros<br></li>
+        <li> Motor: Gasolina V8 de 4.4 litros biturbo de 530 CV (P530)<br></li>
+        <li> Cambio: Automático de 8 velocidades y tracción total</li></ul>
+        ";
+        $datos[6] = "./img/seatleon.jpg"."||"."Seat Leon <br>$482.900"."||"."<ul>
+        <li>Velocidad máxima: 221 km/h<br></li>
+        <li>Aceleración: 0-100 km/h	8,4 s<br></li>
+        <li>Consumo (urbano/extraurbano/mixto): 5,6 l/100 km<br></li>
+        <li>Largo/Ancho/Alto: 4.368 / 1.800 / 1.456 mm<br></li>
+        <li>Peso	1.361 kg<br></li>
+        <li>Carga máxima: 1.890 kg<br></li>
+        <li>Depósito: 50 litros<br></li>
+        <li>Maletero max: 1.301 litros<br></li>
+        <li>Plazas: 5<br></li>
+        <li>Número de cilindros: 4<br></li>
+        <li>Tipo de motor: Híbrido<br></li>
+        <li>Combustible: Gasolina<br></li>
+        <li>Cilindrada: 1.498 cc<br></li>
+        <li>Potencia combustión: 150 cv / 6.000 rpm</li></ul>";
+        $datos[7] = "./img/yaris.webp"."||"."Toyota Yaris <br>$338.900"."||"."<ul>
+        <li>Distancia libre al suelo: 135 (mm)<br></li>
+        <li>Peso bruto: 1550 (kg)<br></li>
+        <li>Capacidad del tanque: 42 L<br></li>
+        <li>Tipo de combustible: Gasolina<br></li>
+        <li>Motor: 1496 16V DOHC, 4cyl Dual VVT-i<br></li>
+        <li>Potencia Hp: 106@6000<br></li>
+        <li>Pasajeros: 5<br></li>
+        <li>Llantas: 185/60 R15</li></ul>";
+        $datos[8] = "./img/m2.jpg"."||"."BMW m2 <br>$1,380,000"."||"."<ul>
+        <li>Combustible: Gasolina<br></li>
+        <li>Cilindrada: 2979 cc<br></li>
+        <li>Potencia: 410/7000 hp/rpm<br></li>
+        <li>Torque: 343/2350-5230 lb·pie/rpm<br></li>
+        <li>Cilindros: 6 en línea<br></li>
+        <li>Válvulas: 24<br></li>
+        <li>Alimentación: inyección directa turbo intercooler<br></li>
+        <li>Aceleración : 0-100 km/h	4.4 s<br></li>
+        <li>Motor - Tracción:	delantero - trasera<br></li>
+        <li>Transmisión: manual 6 velocidades<br></li>
+        <li>Frenos (Del. - Tras.): discos ventilados - discos ventilados</li></ul>
+        ";
+
+        return $datos;
+    }
+
+?>
---
+++ dependecias.php
@@ -0,0 +1,13 @@
+<meta charset="utf-8">
+<link rel="stylesheet" href="./public/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="./public/gridder/css/jquery.gridder.min.css">
+<!-- <link rel="stylesheet" href="./public/font-awesome/css/font-awesome.css"> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="./public/css/style.css">
+
+<link rel="stylesheet" href="./public/css/demo.css">
+
+
+<script src="./public/jquery-3.3.1.min.js"></script>
+<script src="./public/bootstrap/bootstrap.min.js"></script>
+<script src="./public/gridder/js/jquery.gridder.js"></script>
